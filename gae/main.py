da730c2c5fa8a4ca70ebdcb9f771e8c28030fa57
Peter Griess <pg@std.in>
2012-04-14 17:40:22 -0500

    Lots of updates.
    - Create fragment templates to be used in all pages including header,
      footer, etc. Add these to all pages.
    - Get rid of login requirement for all pages in app.yaml; replace with
      decorator and make / bi-modal (302s to /list if logged-in)
    - Make LESS stylesheets media="all" so that we get them even when being
      printed

--- gae/main.py
+++ gae/main.py
@@ -9,35 +9,81 @@ import os
 from urlparse import urlparse
 import webapp2
 
+def login_required(f):
+    '''
+    Decorator to indicate that the given function requires a logged-in user.
+    This should be applied to get()/post()/etc methods in RequestHandler
+    objects.
+    '''
+
+    def wrapper_f(self, *args, **kwargs):
+        if not users.get_current_user():
+            self.redirect(users.create_login_url(self.request.url))
+            return
+
+        f(self, *args, **kwargs)
+
+    return wrapper_f
+
+
+def render_template(path, **kwargs):
+    '''
+    Render a temlate at the given path, creating a dictionary out of
+    the specified keyword arguemnts.
+    '''
+
+    ctx = {}
+    if users.get_current_user():
+        ctx['logout_url'] = users.create_logout_url('/')
+    else:
+        ctx['login_url'] = users.create_login_url('/')
+
+    ctx.update(kwargs)
+
+    return jinja_env.get_template(path).render(ctx)
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        if users.get_current_user():
+            self.redirect('/list')
+            return
+
+        self.response.out.write(render_template('main.html'))
+
 
 class ListHandler(webapp2.RequestHandler):
     def get(self):
         recipes = Recipe.all()
         recipes.filter('user_id = ', users.get_current_user().user_id())
         tmpl = jinja_env.get_template('list.html')
-        self.response.out.write(tmpl.render({
-            'recipes': recipes,
-            'base_url': self.request.host_url}))
+        self.response.out.write(
+            render_template('list.html',
+                recipes=recipes,
+                base_url=self.request.host_url))
 
 
 class ClipHandler(webapp2.RequestHandler):
+    @login_required
     def get(self):
         url = self.request.get('url')
         recipe = scrape(url=url, user_id=users.get_current_user().user_id())
         if not recipe:
             logging.info('Failed to scrape ' + url)
-        tmpl = jinja_env.get_template('clip.html')
-        self.response.out.write(tmpl.render({
-            'recipe': recipe,
-            'url': urlparse(url)}))
+        self.response.out.write(
+            render_template('clip.html',
+                recipe=recipe,
+                url=urlparse(url)))
 
 
 class ViewHandler(webapp2.RequestHandler):
+    @login_required
     def get(self):
         key = db.Key(self.request.get('id'))
         recipe = Recipe.get(key)
-        tmpl = jinja_env.get_template('view.html')
-        self.response.out.write(tmpl.render({'recipe': recipe}))
+        self.response.out.write(
+            render_template('view.html',
+                recipe=recipe))
 
 
 class AddHandler(webapp2.RequestHandler):
@@ -45,6 +91,7 @@ class AddHandler(webapp2.RequestHandler):
     API handler that adds a recipe described by a JSON blob.
     '''
 
+    @login_required
     def post(self):
         recipe = Recipe.from_json(self.request.get('recipe'))
        
@@ -61,6 +108,7 @@ class ScrapeHandler(webapp2.RequestHandler):
     API handler that scrapes a URL and returns the found recipe as a JSON blob.
     '''
 
+    @login_required
     def get(self):
         url = self.request.get('url')
 
@@ -75,7 +123,8 @@ class ScrapeHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-        ('/', ListHandler),
+        ('/', MainHandler),
+        ('/list', ListHandler),
         ('/clip', ClipHandler),
         ('/view', ViewHandler),
         ('/api/scrape', ScrapeHandler),
